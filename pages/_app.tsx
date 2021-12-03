103be5cf17c86ffadbc990571a75ba21ec0ec72d
= <liam2robertson@gmail.com>
2021-12-03 23:59:26 +0000

    ✨ add <head> page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Head><title>Braincache</title></Head>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
