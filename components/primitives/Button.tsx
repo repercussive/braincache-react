8f4d47d32ce4cd60f6bf31af3d5becbbc89dc8c6
= <liam2robertson@gmail.com>
2021-12-04 16:11:05 +0000

    💄 improve Button style

--- components/primitives/Button.tsx
+++ components/primitives/Button.tsx
@@ -6,28 +6,35 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(function Button(props,
   return (
     <button
       sx={{
+        position: 'relative',
+        top: 0,
         cursor: 'pointer',
         userSelect: 'none',
         height: '2.5rem',
         margin: 0,
         paddingX: '1rem',
         border: 'solid 2px',
-        borderBottomWidth: '5px',
         borderRadius: 'default',
         borderColor: 'buttonBorder',
+        backgroundColor: 'buttonBackground',
+        boxShadow: '0 3px 0 var(--button-border-color)',
         color: 'inherit',
         fontSize: 'inherit',
         fontFamily: 'inherit',
         fontWeight: 450,
         lineHeight: '1rem',
-        backgroundColor: 'buttonBackground',
+        transition: 'all 50ms',
         '&:hover, &:active': {
-          borderColor: 'buttonBorderHighlight'
+          borderColor: 'buttonBorderHighlight',
+          boxShadow: '0 3px 0 var(--button-border-color-highlight)',
         },
         '&:active': {
-          marginTop: '5px',
-          height: 'calc(2.5rem - 5px)',
-          borderBottomWidth: '2px'
+          top: '3px',
+          boxShadow: 'none'
+        },
+        '&:disabled': {
+          pointerEvents: 'none',
+          opacity: 0.5
         }
       }}
       ref={ref}
